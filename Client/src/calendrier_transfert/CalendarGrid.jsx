9a2b4678d1402db6189e0d4ec6528a049e9c0123
addiayoub <addiayoub2005@gmail.com>
2025-04-14 16:25:47 +0100

    fonction nouveau

--- Client/src/calendrier_transfert/CalendarGrid.jsx
+++ Client/src/calendrier_transfert/CalendarGrid.jsx
@@ -387,15 +387,7 @@ const CalendarGrid = ({
               <polyline points="20 6 9 17 4 12"/>
             </svg>
           </button>
-          ${!transfer.showBoxIcon && !transfer.isGroup ? `
-          <button id="delete-btn" class="bg-transparent border text-white w-12 h-12 rounded-full flex items-center justify-center hover:bg-red-500/20">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash">
-              <path d="M3 6h18"/>
-              <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/>
-              <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
-            </svg>
-          </button>
-          ` : ''}
+          
         </div>
       `,
       didRender: () => {
